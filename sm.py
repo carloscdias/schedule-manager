1a10b7356c2db704209b3445b8e0b3b39a4e0138
Carlos Cardoso Dias <ccd.leron@gmail.com>
2016-09-28 23:17:54 -0300

    Alterado nome do parametro opcional modo -> metodo

--- sm.py
+++ sm.py
@@ -8,13 +8,13 @@ def main():
 	parser = ArgumentParser( description = "Monta a grade de horarios de disciplinas para um aluno da UERJ atraves de sua matricula de acordo com o metodo de alocacao escolhido" )
 	
 	parser.add_argument( 'matricula', help = "Matricula do aluno" )
-	parser.add_argument( '-m', '--modo', default = 'completo', choices = [ 'completo', 'continuo', 'credito' ], help = "Modo de alocacao de disciplinas" )
+	parser.add_argument( '-m', '--metodo', default = 'completo', choices = [ 'completo', 'continuo', 'credito' ], help = "Metodo de alocacao de disciplinas" )
 
 	args = parser.parse_args()
 
-	# Modo de alocacao 'completo': prioriza o maior numero de disciplinas que podem ser alocadas
-	# Modo de alocacao 'continuo': prioriza o maior numero de disciplinas que podem ser alocadas sem que haja tempos livres na grade
-	# Modo de alocacao 'credito': prioriza alocar as disciplinas que possuem o maior numero de creditos
+	# completo	- prioriza o maior numero de disciplinas que podem ser alocadas
+	# continuo	- prioriza o maior numero de disciplinas que podem ser alocadas sem que haja tempos livres na grade
+	# credito	- prioriza alocar as disciplinas que possuem o maior numero de creditos
 
 	print( 'Matricula:', args.matricula )
 	print( 'Modo de alocacao de disciplinas:', args.modo )
