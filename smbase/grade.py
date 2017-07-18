ad49ac00d211f83df0bb134f334735f91371a469
Jhonatan Gomes <jhonatan.gomes93@gmail.com>
2017-07-18 15:20:14 -0300

    criado monta grade por metodo aleatorio

--- smbase/grade.py
+++ smbase/grade.py
@@ -9,7 +9,7 @@ class Grade:
         Exemplo: ("seg", "m2") = "Calculo"
     """
 
-    def __init__(self, disciplinas = None):
+    def __init__(self, disciplinas = None, metodo = None):
         """Construtor
             Inicializa a grade de horários com uma lista de disciplinas
 
@@ -24,8 +24,35 @@ class Grade:
                     self._grid[( dia, char_horario + str( numero_horario ) )] = ''
 
         if disciplinas:
-            for d in disciplinas:
+            if metodo == 'aleatorio':
+                self.monta_grade_metodo_aleatorio(disciplinas)
+            elif metodo == 'completo':
+                self.monta_grade_metodo_completo(disciplinas)
+            elif metodo == 'credito':
+                self.monta_grade_metodo_credito(disciplinas)
+            elif metodo == 'continuo':
+                self.monta_grade_metodo_continuo(disciplinas)
+
+    def monta_grade_metodo_completo(self, disciplinas):
+        pass
+
+    def monta_grade_metodo_continuo(self, disciplinas):
+        pass
+
+    def monta_grade_metodo_credito(self, disciplinas):
+        pass
+
+    def monta_grade_metodo_aleatorio(self, disciplinas):
+        for d in disciplinas:
+            if self.pode_incluir(d):
                 self.add_disciplina(d)
+
+    def pode_incluir(self, disciplina):
+        for dia in disciplina._horarios:
+            for hr in disciplina._horarios[dia]:
+                if self._grid[(dia, hr)] != "":
+                    return False
+        return True
 
     def add_disciplina(self, disciplina):
         """Adiciona uma disciplina na grade
